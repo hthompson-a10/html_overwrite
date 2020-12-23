9a591b9d0135a8ebd024a888fd405daca2be5afc
Hunter Thompson <thompson.grey.hunter@gmail.com>
2020-12-23 15:39:05 -0800

    Refactored gen class into gen function

--- tool.py
+++ tool.py
@@ -4,32 +4,18 @@ import re
 import sys
 
 
-class html_list(object):
-
-    def __init__(self, directory_path):
-        # os.walk returns a 3-tuple of (dirpath, dirnames, filenames)
-        self.filenames = next(os.walk(directory_path))[2]
-        self.max_index = len(self.filenames) - 1
-        self.curr_index = -1
-
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        return self.next()
-
-    def next(self):
-        if self.curr_index < self.max_index:
-            self.curr_index += 1
-            file_name = self.filenames[self.curr_index]
+def html_list(directory_path):
+        filenames = next(os.walk(directory_path))[2]
+        max_index = len(filenames)
+        curr_index = 0
+        while curr_index < max_index:
+            file_name = filenames[curr_index]
             if re.search(".*\.html$", file_name):
-                return file_name
-            else:
-                self.next()
-        raise StopIteration()
+                yield file_name
+            curr_index += 1
+            
 
-
-def overwrite_file(file_name, element, num_elem):
+def overwrite_file(file_name, element):
     """
     Credit to https://stackoverflow.com/questions/11469228/replace-and-overwrite-instead-of-appending
     for the seek() and truncate() idea.
@@ -40,9 +26,9 @@ def overwrite_file(file_name, element, num_elem):
     with open(file_name, "r+") as f:
         raw_data = f.read()
         file_soup = bs4.BeautifulSoup(raw_data, features="html.parser")
-        elem_foud = file_soup.find_all(element)[:num_elem]
+        elem_found = file_soup.find_all(element)[0]
         f.seek(0)
-        f.write(str(elem_foud))
+        f.write(str(elem_found))
         f.truncate()
 
 
@@ -53,6 +39,6 @@ def main():
         Most likely desired is `` python tool.py . 'table' 1 ``
     """
     for file_name in html_list(sys.argv[1]):
-        overwrite_file(file_name, sys.argv[2], int(sys.argv[3]))
+        overwrite_file(file_name, sys.argv[2])
 
 main()
